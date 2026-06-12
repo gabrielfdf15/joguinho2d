debc2702b7a41c999b51b5e6e219ef7cbfdbe7a1
JairBolsonaro-King <gabrielfdf15@gmail.com>
2026-06-12 16:08:34 -0300

    moeda counter

---
+++ Assets/Points.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Points : MonoBehaviour
+{
+    float points=0;
+
+    public TextMeshProUGUI scoreText;
+
+    void Start(){
+        
+        scoreText.text = "0";
+    }
+
+
+
+    public void AddPoints()
+    {
+        points++;
+        scoreText.text = points.ToString();
+}
+}
