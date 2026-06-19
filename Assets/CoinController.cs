dac45ff1fef8565a6e76ee02ecb037a6d72939c7
José <gabrielfdf15@gmail.com>
2026-06-19 14:55:59 -0300

    calma

--- Assets/CoinController.cs
+++ Assets/CoinController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
-   private void OnTriggerEnter2D(Collider2D other){
-    if(other.CompareTag("Player")){
-        Destroy(this.gameObject);
+    public CircleCollider2D Collider2D;
+    public PersonagemController  personagemController;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            personagemController.AddToPoints(1);
+            Destroy(this.gameObject);
+            
+        }
     }
-   }
-   }
+
+}
--- Assets/PersonagemController.cs
+++ Assets/PersonagemController.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class PersonagemController : MonoBehaviour
 {
     public GameObject groundCheck;
     public Rigidbody2D rb2d; 
     public float vel;
     public float jumpForce;
+    public TMP_Text pontuacao;
+    private int points = 0;
+    public void AddToPoints(int x)
+    {
+        points += x;
+        pontuacao.text = "Pontuação: " + points.ToString();
+    }
     // Start is called before the first frame update
-        private GroundCheck groundCheckScript;    
+    private GroundCheck groundCheckScript;    
 
     void Start()
     {
         rb2d = this.GetComponent<Rigidbody2D>();
         groundCheckScript = groundCheck.GetComponent<GroundCheck>();
+        pontuacao.text = "Pontuação: 0";
     }
 
     // Update is called once per frame
